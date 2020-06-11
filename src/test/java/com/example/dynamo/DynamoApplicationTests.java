4372e7b8a3b09869161df0dbad79ccf5ca122bba
Mohit Kumar <mkumar@tekion.com>
2020-06-11 22:15:48 +0530

    Initial commit

---
+++ src/test/java/com/example/dynamo/DynamoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dynamo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
